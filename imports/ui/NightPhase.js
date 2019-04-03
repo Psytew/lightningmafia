fab9462576e847ac02c9fc3d0cb410dc372d605d
Patrick <patrickwestervelt.us@gmail.com>
2019-04-03 12:48:05 -0400

    Added Forms and Information for Night Phase

--- imports/ui/NightPhase.js
+++ imports/ui/NightPhase.js
@@ -2,10 +2,137 @@ import React from 'react'
 
 import {Users} from '../users.js'
 
+function mapUsers(userInfo){
+	thisUser = Users.find({_id:userInfo}).fetch()[0]
+	users = Users.find({room:thisUser.room}).fetch()
+	return users.map((user) => {
+		if (user.name != null){
+			return <option value={user.name} key={user._id}>{user.name}</option>
+		}
+	}
+)}
+
 export default class WaitingRoom extends React.Component {
 	render(){
-		return (
-			<p>This is the Night Phase!</p>
-		)
+		var thisUser = Users.find({_id:this.props.userID}).fetch()[0]
+		var werewolves = Users.find({room:room,role:"Werewolf"}).fetch()
+		var werewolfMessage = ""
+		var minionMessage = ""
+		if (werewolves.length == 0){
+			minionMessage = "There is no werewolf! Try to survive!"
+		} else if (werewolves.length == 1){
+			werewolfMessage = "You are the only werewolf!"
+			minionMessage = werewolves[0].name + " is the only werewolf!"
+		} else {
+			werewolfMessage = "The werewolves are: "
+			for (let i = 0; i < werewolves.length - 1; i++){
+				werewolfMessage += werewolves[i].name + ", "
+			}
+			werewolfMessage += "and " + werewolves[werewolves.length-1].name + "!"
+			minionMessage = werewolfMessage
+		}
+		var masons = Users.find({room:room,role:"Mason"}).fetch()
+		var masonMessage = ""
+		if (masons.length == 1){
+			masonMessage = "You are the only mason! How sad."
+		} else {
+			masonMessage += "The masons are: "
+			for (let i = 0; i < masons.length - 1; i++){
+				masonMessage += masons[i].name + ", "
+			}
+			masonMessage += "and " + masons[masons.length-1].name + "!"
+		}
+		if (thisUser.role == "Seer"){
+			return (
+				<div>
+					<h2>Night Phase</h2>
+					<p>Your role is the {thisUser.role}</p>
+					<p>Who do you want to investigate?</p>
+					<form>
+						<select name="seer">
+						  {mapUsers(this.props.userID)}
+						</select>
+						<button>Submit</button>
+					</form>
+				</div>
+			)
+		} else if (thisUser.role == "Werewolf"){
+			return (
+				<div>
+					<h2>Night Phase</h2>
+					<p>Your role is the {thisUser.role}</p>
+					<p>{werewolfMessage}</p>
+				</div>
+			)
+		} else if  (thisUser.role == "Villager"){
+			return (
+				<div>
+					<h2>Night Phase</h2>
+					<p>Your role is the {thisUser.role}</p>
+					<p>You just get to chill out!</p>
+				</div>
+			)
+		} else if (thisUser.role == "Robber"){
+			return (
+				<div>
+					<h2>Night Phase</h2>
+					<p>Your role is the {thisUser.role}</p>
+					<p>Who do you want to rob?</p>
+					<form>
+						<select name="robber">
+						  {mapUsers(this.props.userID)}
+						</select>
+						<button>Submit</button>
+					</form>
+				</div>
+			)
+		} else if (thisUser.role == "Troublemaker"){
+			return (
+				<div>
+					<h2>Night Phase</h2>
+					<p>Your role is the {thisUser.role}</p>
+					<p>Who do you want to switch?</p>
+					<form>
+						<select name="troublemaker1">
+						  {mapUsers(this.props.userID)}
+						</select>
+						<select name="troublemaker2">
+						  {mapUsers(this.props.userID)}
+						</select>
+						<button>Submit</button>
+					</form>
+				</div>
+			)
+		} else if (thisUser.role == "Mason"){
+			return (
+				<div>
+					<h2>Night Phase</h2>
+					<p>Your role is the {thisUser.role}</p>
+					<p>{masonMessage}</p>
+				</div>
+			)
+		} else if (thisUser.role == "Bodyguard"){
+			return (
+				<div>
+					<h2>Night Phase</h2>
+					<p>Your role is the {thisUser.role}</p>
+					<p>Who do you want to protect?</p>
+					<form>
+						<select name="bodyguard">
+						  {mapUsers(this.props.userID)}
+						</select>
+						<button>Submit</button>
+					</form>
+				</div>
+			)
+		} else if (thisUser.role == "Minion"){
+			return (
+				<div>
+					<h2>Night Phase</h2>
+					<p>Your role is the {thisUser.role}</p>
+					<p>{minionMessage}</p>
+				</div>
+			)
+		}
 	}
 }
--- server/main.js
+++ server/main.js
@@ -11,11 +11,28 @@ Meteor.startup(() => {
   console.log(Users.find().fetch());
   console.log("Hi!")
 
-  Meteor.methods({
-      StartButtonFunction(userInfo){
-			room = Users.find({_id:userInfo}).fetch()[0].room
-			console.log(room)
-			console.log(Users.find({room:room}).fetch())
+  	Meteor.methods({
+      	StartButtonFunction(userInfo){
+      		//The roles for the game
+      		let roles = ["Villager","Werewolf","Seer","Robber","Bodyguard","Troublemaker","Werewolf","Mason","Mason","Minion","Civilian","Mason"]
+      		//Finds the current room, and then room length; gets only as many roles as needed
+			let room = Users.find({_id:userInfo}).fetch()[0].room
+			let rolesToUse = roles.slice(0,Users.find({room:room}).fetch().length + 2)
+			//Shuffles the order of the roles
+			var currentIndex = rolesToUse.length, temporaryValue, randomIndex;
+			while (0 !== currentIndex) {
+		    randomIndex = Math.floor(Math.random() * currentIndex);
+		    currentIndex -= 1;
+		    temporaryValue = rolesToUse[currentIndex];
+		    rolesToUse[currentIndex] = rolesToUse[randomIndex];
+		    rolesToUse[randomIndex] = temporaryValue;
+		  	}
+		  	//Assigns the role to each player
+		  	let users = Users.find({room:room}).fetch()
+		  	for (let i = 0; i < users.length; i++){
+		  		Users.update({_id:users[i]._id},{$set:{role:rolesToUse[i]}})
+		  	}
+		  	//Updates each player to go the night phase
 			Users.update({room:room},{$set: {gameStatus:"night"}},{multi:true})
 		}
     });
