1089b7f18ead33905fd05b8a4abb6c0db0b5246c
Patrick <patrickwestervelt.us@gmail.com>
2019-04-07 00:20:49 -0400

    Added Second Button

--- imports/ui/Point.js
+++ imports/ui/Point.js
@@ -8,7 +8,8 @@ export default class Point extends React.Component {
 			<div>
 				<h1 className="Header Header--lessMargin"><i className="fas fa-bolt"> </i>Lightning Mafia<i className="fas fa-user-secret"> </i></h1>
 				<h1 className="WaitingInfo">Vote! Point for who hangs!</h1>
-				<button className="NightButton" onClick={this.JumpToResults.bind(this)}>We picked a werewolf!</button>
+				<button className="NightButton" onClick={this.JumpToResults.bind(this)}>We picked a Mafia Member!</button>
+				<button className="NightButton" onClick={this.JumpToResults.bind(this)}>We failed to pick a Mafia Member!</button>
 			</div>
 		)
 	}
